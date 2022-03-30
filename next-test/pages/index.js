8aedc5e7c1b0dc7a5deeb30d242a568b11f2a496
vayazee <vayazee>
2022-03-31 02:57:53 +0900

    vscode commit test

--- next-test/pages/index.js
+++ next-test/pages/index.js
@@ -22,12 +22,12 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a className={styles.card}>
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
